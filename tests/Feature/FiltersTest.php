361bdbe41e23025034092d329da2ba1e61b4ddc7
amir <adada5116@gmail.com>
2021-11-17 11:26:46 +0330

    getters test

---
+++ tests/Feature/FiltersTest.php
@@ -0,0 +1,32 @@
+<?php
+
+namespace AmirHossein5\Routin\Tests\Feature;
+
+use AmirHossein5\Routin\Tests\TestCase;
+
+class FiltersTest extends TestCase
+{
+    public function test_uri_starts_with_filters_routes_correctly()
+    {
+    }
+
+    public function test_uri_ends_with_filters_routes_correctly()
+    {
+    }
+
+    public function test_name_starts_with_filters_routes_correctly()
+    {
+    }
+
+    public function test_name_ends_with_filters_routes_correctly()
+    {
+    }
+
+    public function test_without_parameter_filters_routes_correctly()
+    {
+    }
+
+    public function test_method_filters_routes_correctly()
+    {
+    }
+}
---
+++ tests/TestCase.php
@@ -0,0 +1,116 @@
+<?php
+
+namespace AmirHossein5\Routin\Tests;
+
+use AmirHossein5\Routin\RoutinServiceProvider;
+use AmirHossein5\Routin\Tests\Controllers\UserController;
+
+class TestCase extends \Orchestra\Testbench\TestCase
+{
+    /**
+     * Routes defined for testing.
+     * 
+     */
+
+    protected $routes = [
+        'get' => [
+            'user' => [
+                'name' => "user.index",
+                'parameters' => []
+            ],
+            'user/create' => [
+                'name' => "user.create",
+                'parameters' => []
+            ],
+            'user/{user}' => [
+                'name' => "user.edit",
+                'parameters' => [
+                    'user'
+                ]
+            ],
+            'user/show' => [
+                'name' => "user.show",
+                'parameters' => [
+                    'user'
+                ]
+            ],
+        ],
+
+        'post' => [
+            'user' => [
+                'name' => "user.store",
+                'parameters' => []
+            ]
+        ],
+
+        'put' => [
+            'user/{user}' => [
+                'name' => "user.update",
+                'parameters' => [
+                    'user'
+                ]
+            ]
+        ],
+
+        'delete' => [
+            'user/{user}' => [
+                'name' => "user.destroy",
+                'parameters' => [
+                    'user'
+                ]
+            ]
+        ],
+
+        'names' => [
+            "user.index",
+            "user.create",
+            "user.store",
+            "user.show",
+            "user.edit",
+            "user.update",
+            "user.destroy"
+        ],
+
+        'uris' => [
+            'user',
+            'user/create',
+            'user',
+            'user/{user}',
+            'user/{user}/edit',
+            'user/{user}',
+            'user/{user}'
+        ],
+
+        'parameters' => [
+            "user.index" => [],
+            "user.create" => [],
+            "user.store" => [],
+            "user.show" => ['user'],
+            "user.edit" => ['user'],
+            "user.update" => ['user'],
+            "user.destroy" => ['user']
+        ]
+    ];
+
+    public function setUp(): void
+    {
+        parent::setUp();
+    }
+
+    protected function getPackageProviders($app)
+    {
+        return [
+            RoutinServiceProvider::class,
+        ];
+    }
+
+    protected function getEnvironmentSetUp($app)
+    {
+        //
+    }
+
+    protected function defineRoutes($router)
+    {
+        $router->resource('user', UserController::class);
+    }
+}
