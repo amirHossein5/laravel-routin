00278292aa7ae3ded2d0b2043e8b5cb18b051141
amir <adada5116@gmail.com>
2021-11-17 13:04:54 +0330

    add unit test for github actions

---
+++ tests/Unit/ExampleTest.php
@@ -0,0 +1,13 @@
+<?php
+
+namespace AmirHossein5\Routin\Tests\Unit;
+
+use AmirHossein5\Routin\Tests\TestCase;
+
+class ExampleTest extends TestCase
+{
+    public function test()
+    {
+        $this->assertTrue(true);
+    }
+}
