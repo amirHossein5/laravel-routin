1427ceb7f6cc4afc2e668bd72a6632704cf62a51
Amir Hossein <68776630+amirHossein5@users.noreply.github.com>
2022-02-06 11:39:45 +0330

    Update RoutinServiceProvider.php

--- src/RoutinServiceProvider.php
+++ src/RoutinServiceProvider.php
@@ -9,10 +9,6 @@ class RoutinServiceProvider extends ServiceProvider
 {
     public function register()
     {
-        $this->app->bind('calculator', function ($app) {
-            return new Calculator();
-        });
-        
         $this->app->bind('routin', function ($app) {
             return new Routin();
         });
